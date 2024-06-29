ba7e646b7d9ef03016ff9900cf2c809fa51ad141
kajipan <rskajipan1101@gmail.com>
2024-06-30 01:14:55 +0530

    props

--- src/components/greet.js
+++ src/components/greet.js
@@ -1,10 +1,15 @@
 import React from "react"
 
-function Greet() {
-    return (<h1>Hello kaji</h1>
-        
-    )
-
+const Greet = props => {
+  console. log(props)
+  return (
+    <div>
+        <h1>
+            hello {props.name} a.k.a {props.heroName}
+        </h1>
+        {props.children}
+    </div>
+  )
 }
 
 export default Greet
