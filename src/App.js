2d8087b4bfc9c1e3f11d31d4a9232dde6bcb7e42
kajipan <rskajipan1101@gmail.com>
2024-07-04 18:32:40 +0530

    Event Handling

--- src/App.js
+++ src/App.js
@@ -6,10 +6,14 @@ import Welcome from './components/welcome';
 import Hello from './components/hello';
 import Message from './components/message';
 import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <FunctionClick/>
+      <ClassClick/>
+      {/* <Counter /> }
       {/* <Message/> */}
       {/* <Greet name="Bruce" heroName="Batman">
         <p>This is children props</p>
@@ -17,7 +21,7 @@ function App() {
       <Greet name="Clark " heroName="Superman">
         <button>Action</button>
       </Greet> */}
-      <Greet name="Diana " heroName="wonderwomen"/> 
+      {/* <Greet name="Diana " heroName="wonderwomen"/> 
       <Welcome name="Bruce" heroName="Batman"/> 
       {/*<Welcome name="Clark " heroName="Superman"/>
       <Welcome name="Diana " heroName="wonderwomen"/> */}
