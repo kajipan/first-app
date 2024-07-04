63f544a8bc49289a690b28af73b253228a719d3e
kajipan <rskajipan1101@gmail.com>
2024-07-04 19:53:45 +0530

    binding event handlers

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ import Message from './components/message';
 import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
 function App() {
   return (
     <div className="App">
-      <FunctionClick/>
-      <ClassClick/>
+      <EventBind/>
+      {/*<FunctionClick/> }
+      {/*<ClassClick/> }
       {/* <Counter /> }
       {/* <Message/> */}
       {/* <Greet name="Bruce" heroName="Batman">
